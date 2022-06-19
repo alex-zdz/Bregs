b89297a69d6bc9360c3b3afecb3cb9e529209eb7
alex-zdz <63648770+alex-zdz@users.noreply.github.com>
2022-06-19 16:05:12 +0200

    Initial commit

---
+++ R/nullreg.R
@@ -0,0 +1,120 @@
+nullreg<-function(X,Y,
+                  nburn,nthin,n_save,
+                  prior.mean.beta=NULL, prior.var.beta=NULL, prior.sig2=NULL,
+                  sig2.samp= NULL, beta.samp = NULL){
+
+
+
+  ## extract dimensions from design matrix
+
+  if(length(dim(X)) == 3){
+    stop("Not yet implemented for panel data", call.=FALSE)
+    I<-dim(X)[1]
+    Ttot<-dim(X)[2]
+    p<-dim(X)[3]
+  }else{
+    I<-dim(X)[1]
+    p<-dim(X)[2]
+  }
+
+  ## Number of MCMC iterations
+  niter <-  nburn+ nthin*n_save
+
+  #### Priors
+  if(is.null(prior.mean.beta)) prior.mean.beta <- rep(0, p)
+  if(is.null(prior.var.beta))  prior.var.beta  <- diag(rep(1, p))
+  if(is.null(prior.sig2)) prior.sig2 <- c(1, 0.01)
+
+  data.precision.beta <- crossprod(X.standardised)
+  if(length(prior.var.beta)==1){
+    prior.precision.beta <- 1 / prior.var.beta
+  }else{
+    prior.precision.beta <- diag(1/prior.var.beta)
+  }
+
+
+  ###########################
+  ## Initial parameter values
+  ###########################
+  mod.glm   <- glm(Y ~ X)
+  beta0     <- mod.glm$coefficients[1]
+  beta.mean <- mod.glm$coefficients[-1]
+  beta.sd <- sqrt(diag(summary(mod.glm)$cov.scaled))[-1]
+  beta.samp <- rnorm(n=length(beta.mean), mean=beta.mean, sd=beta.sd)
+
+  res.temp <- Y - beta0 - X %*% beta.samp
+  res.sd <- sd(res.temp, na.rm=TRUE)
+  sig2.samp <- res.sd/2
+
+
+  ###########################
+  ## Results storage
+  ###########################
+
+  beta_out <- vector("list", n_save)
+  sig2_out <- rep(NA, n_save)
+
+  ###########################
+  ## pre-calculated values:
+  ###########################
+  pre1 <- crossprod(X) + prior.precision.beta
+  pre2 <- crossprod(X, Y) + prior.precision.beta %*% prior.mean.beta
+  pre3 <- prior.sig2[1] + I/2 + p/2
+
+  ###########################
+  ## Run the model
+  ###########################
+  ## Start timer
+  if(verbose) {
+    cat("Generating", n.keep, "post burnin and thinned (if requested) samples.\n", sep = " ")
+    progressBar <- txtProgressBar(style = 3)
+    percentage.points<-round((1:100/100)*n.sample)
+  } else {
+    percentage.points<-round((1:100/100)*n.sample)
+  }
+
+
+  ###########################
+  ## Start Gibbs
+  ###########################
+
+  for (iter in 1:niter){
+
+  # Sample beta:
+  Sigma <- solve(pre1/sig2.samp)
+  mu <- Sigma %*% (pre2/sig2.samp)
+  beta.samp <- mvtnorm::rmvnorm(1, mu, Sigma)
+
+  # Sample sigma^2:
+  tmp <- C0 + .5*(crossprod(y - X%*%beta.samp) +
+                    t(beta.samp-b0) %*% B0inv %*% (beta.samp-b0))
+  sig2.samp <- 1/rgamma(1, pre3, rate=tmp)
+
+
+
+  if(iter > nburn && ((iter - nburn) %% nthin) == 0){
+    iter_aux <- (iter- nburn)/nthin
+    beta_out[[iter_aux]] <- beta.samp
+    sig2_out[iter_aux]<-sig2.samp
+  }
+
+  }
+
+  return(list(beta_out = beta_out,sig2_out=sig2_out))
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
